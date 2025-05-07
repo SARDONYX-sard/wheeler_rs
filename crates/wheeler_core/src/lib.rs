fba521c8ffdcc35abfa02c287114f6d3f2f8d6ef
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2025-05-07 22:44:01 +0900

    test: add dummy test to prevent CI errors for missing tests

--- crates/wheeler_core/src/lib.rs
+++ crates/wheeler_core/src/lib.rs
@@ -1,9 +1,3 @@
 pub mod rendering;
 pub mod user_input;
 pub mod wheeler;
-
-// This is because CI will treat it as an error if there is at least one test missing.
-#[test]
-fn dummy_test() {
-    assert_eq!(1 + 1, 2);
-}
--- crates/wheeler_rs/src/lib.rs
+++ crates/wheeler_rs/src/lib.rs
@@ -22,3 +22,9 @@ fn skse_event_listener(message: &Message) {
         if msg_type == MessageType::PostLoadGame {}
     }
 }
+
+// This is because CI will treat it as an error if there is at least one test missing.
+#[test]
+fn dummy_test() {
+    assert_eq!(1 + 1, 2);
+}
